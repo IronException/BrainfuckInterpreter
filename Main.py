49484387b4b2feeba58240bdd173b8a59a9d4b81
IronException <mail-fuer-friedolin@web.de>
2019-01-18 12:36:01 +0000

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 
-
 class Brainfuck(object):
 
     def setMemory(self, memory):
@@ -10,7 +9,7 @@ class Brainfuck(object):
 
     oldCode = ""
 
-    index = 1
+    index = 0
     variables = []
     waitForCommand = ""
 
@@ -23,7 +22,7 @@ class Brainfuck(object):
                 if(command.isdigit()):
                     self.extra += command
                 else:
-                    self.setCurrent(self.extra)
+                    self.setCurrent(int(self.extra))
                     self.extra = ""
                     self.waitForCommand = ""
             elif(self.waitForCommand == self.BRACKET):
@@ -38,8 +37,12 @@ class Brainfuck(object):
             if(self.getCurrent() == 0):
                 self.waitForCommand = self.BRACKET
         elif(command == ']'):
+            print("[ " + str(self.getCurrent()))
+
+
             if(self.getCurrent() != 0):
                 self.redoCode()
+                print("well")
         elif(command == '+'):
             self.setCurrent(self.getCurrent() + 1)
         elif(command == '-'):
@@ -54,40 +57,64 @@ class Brainfuck(object):
             self.executeCommand(c)
 
 
-    def getIntex(self):
+    def getIndex(self):
         while(self.index < 0):
-            self.index += self.variables.lenth
-        self.index %= self.variables.lenth
-        return self.index
+            self.index += len(self.variables)
+        self.index %= len(self.variables)
+        return int(self.index)
 
     def getCurrent(self):
         return self.variables[self.getIndex()]
 
 
     def setCurrent(self, value):
-         self.variables[self.getIndex] = value
+         self.variables[self.getIndex()] = value
 
 
     def redoCode(self):
-        redoIndex = self.oldCode.length - 1
+        redoIndex = len(self.oldCode) - 2
         loopsToPass = 1
         while(True):
             if(self.oldCode[redoIndex] == '['):
                 loopsToPass -= 1
                 if(loopsToPass <= 0):
-                    self.executeCode(self.oldCode[redoIndex:])
+                    self.executeCode(self.oldCode[redoIndex])
                     return
             elif(self.oldCode[redoIndex] == ']'):
                 loopsToPass += 1
 
+            redoIndex -= 1
 
-brainfuck = Brainfuck()
 
-brainfuck.setMemory([0, 0, 0, 0, 0])
 
-brainfuck.executeCode(">>++-.,")
+brainfuck: Brainfuck = Brainfuck()
 
-print(brainfuck.index)
+brainfuck.setMemory([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]) #Hello World!
+
+print("{0}: {1}".format(str(brainfuck.index), str(brainfuck.variables)))
+
+
+#brainfuck.executeCode("++++++++++[>+++++++>++++++++++>+++>+<<<<-]++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
+#brainfuck.executeCode("+>>+++++[-.]<.")
+
+brainfuck.executeCode("++++++++++[>+++++++>++++++++++>+++>+<<<<-
+ ]                       Schleife zur Vorbereitung der Textausgabe
+ >++.                    Ausgabe von
+ >+.                     Ausgabe von
+ +++++++.
+ .
+ +++.
+ >++.                    Leerzeichen
+ <<+++++++++++++++.
+ >.
+ +++.
+ ------.
+ --------.
+ >+.
+ >.                      Zeilenvorschub
+ +++.")
+
+print(str(brainfuck.index) + ": " + str(brainfuck.variables))
 
 
 
